9351454b280c219eed5199b357d01b32e6c3236b
sueKK <125456919+sueKK@users.noreply.github.com>
2025-01-22 12:36:30 +0300

    edit Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
       <Header/>
     </div>
   )
