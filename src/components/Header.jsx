50e0a33451f078b667b15e5bff747951e351e147
sueKK <125456919+sueKK@users.noreply.github.com>
2025-01-22 23:41:48 +0300

    edit Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,13 @@ function Header() {
     flex items-center w-full overflow-hidden' style=
     {{backgroundImage:"url('/header_img.png')"}} id='Header'>
         <Navbar/>
+        <div>
+            <h2>Explore homes that fit your dreams</h2>
+            <div>
+                <a href="">Projects</a>
+                <a href="">Contact Us</a>
+            </div>
+        </div>
     </div>
   )
 }
