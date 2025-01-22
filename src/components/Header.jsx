ee0c0dfa28dbb47cee56c3201ddb54fee4642374
sueKK <125456919+sueKK@users.noreply.github.com>
2025-01-22 23:52:49 +0300

    style Header text in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,9 @@ function Header() {
     flex items-center w-full overflow-hidden' style=
     {{backgroundImage:"url('/header_img.png')"}} id='Header'>
         <Navbar/>
-        <div>
-            <h2>Explore homes that fit your dreams</h2>
+        <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+            <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block
+            max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
             <div>
                 <a href="">Projects</a>
                 <a href="">Contact Us</a>
