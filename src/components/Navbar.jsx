f418bace801b2f28b6cbe429b87ec67c70d178b5
sueKK <125456919+sueKK@users.noreply.github.com>
2025-01-24 18:32:20 +0300

    Add Menu links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 
 const Navbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between
@@ -20,7 +21,25 @@ const Navbar = () => {
             </ul>
             <button className='hidden md:block bg-white px-8 py-2
             rounded-full'>Sign up</button>
-            <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
+            <img src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt="" />
+        </div>
+        {/*---------mobile menu--------*/}
+        <div className='md:hidden fixed w-full right-0 top-0 bottom-0
+        overflow-hidden bg-white transition-all'>
+          <div className='flex justify-end p-6 cursor-pointer'>
+            <img src={assets.cross_icon} className='w-6' alt="" />
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg
+          font-medium'>
+            <a href="#Header" className='px-4 py-2 rounded-full 
+            inline-block'>Home</a>
+             <a href="#About" className='px-4 py-2 rounded-full 
+            inline-block'>About</a>
+             <a href="#Projects" className='px-4 py-2 rounded-full 
+            inline-block'>Projects</a>
+             <a href="#Testimonials" className='px-4 py-2 rounded-full 
+            inline-block'>Testimonials</a>
+          </ul>
         </div>
     </div>
   )
