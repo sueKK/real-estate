cccb4f977c793b4741b70b02685b0308c68df602
sueKK <125456919+sueKK@users.noreply.github.com>
2025-02-03 17:49:19 +0300

    Add About section

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center
+    container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden'
+    id='About'>
+      <h1>About<span>Our Brand</span></h1>
+      <p>Passionate About Properties, Dedicated to Your Vision</p>
+    </div>
+  )
+}
+
+export default About
