d6f420ef98b2b810812acc47e42a53666cc47179
sueKK <125456919+sueKK@users.noreply.github.com>
2025-02-04 18:43:14 +0300

    Style text in About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,9 @@ const About = () => {
     <div className='flex flex-col items-center justify-center
     container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden'
     id='About'>
-      <h1>About<span>Our Brand</span></h1>
-      <p>Passionate About Properties, Dedicated to Your Vision</p>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About<span className=' underline underline-offset-4 decoration-1 under font-light'>Our
+         Brand</span></h1>
+      <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to Your Vision</p>
     </div>
   )
 }
