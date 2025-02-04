1833fa7b4aa7db7a400e95ec0bd953918d6543af
sueKK <125456919+sueKK@users.noreply.github.com>
2025-02-04 18:51:57 +0300

    Design About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
@@ -8,6 +9,9 @@ const About = () => {
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About<span className=' underline underline-offset-4 decoration-1 under font-light'>Our
          Brand</span></h1>
       <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to Your Vision</p>
+      <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+        <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg' />
+      </div>
     </div>
   )
 }
