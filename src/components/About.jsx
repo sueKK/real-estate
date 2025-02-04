133a2b0900e208287d58e992b57c92b6ea9ba7c9
sueKK <125456919+sueKK@users.noreply.github.com>
2025-02-04 19:00:29 +0300

    Add body to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,26 @@ const About = () => {
       <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
         <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg' />
       </div>
+      <div className='flex flex-col items-center md:items-start mt-10 text-gray-600 '>
+        <div className=' grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+            <div>
+                <p className='text 4xl font-medium text-gray-800'>10+</p>
+                <p>Years of Excellence</p>
+            </div>
+            <div>
+                <p className='text 4xl font-medium text-gray-800'>12+</p>
+                <p>Projects Completed</p>
+            </div>
+            <div>
+                <p className='text 4xl font-medium text-gray-800'>20+</p>
+                <p>Mn. Sq. Ft. Delivered</p>
+            </div>
+            <div>
+                <p className='text 4xl font-medium text-gray-800'>25+</p>
+                <p>Ongoing Projects</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
