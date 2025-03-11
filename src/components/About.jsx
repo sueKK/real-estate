e88660c39f0421ffce000482621291f93d6d0674
sueKK <125456919+sueKK@users.noreply.github.com>
2025-03-11 12:15:38 +0300

    Add About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,30 +8,36 @@ const About = () => {
     id='About'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About<span className=' underline underline-offset-4 decoration-1 under font-light'>Our
          Brand</span></h1>
-      <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to Your Vision</p>
-      <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
-        <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg' />
-      </div>
-      <div className='flex flex-col items-center md:items-start mt-10 text-gray-600 '>
-        <div className=' grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
-            <div>
-                <p className='text 4xl font-medium text-gray-800'>10+</p>
-                <p>Years of Excellence</p>
+      <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About
+         Properties, Dedicated to Your Vision</p>
+         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+            <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg'/>
+            <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+              <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+                <div>
+                  <p className='text-4xl font-medium text-gray-800'>10+</p>
+                  <p> Years of Excellence</p>
+                </div>
+                <div>
+                  <p className='text-4xl font-medium text-gray-800'>12+</p>
+                  <p> Projects Completed</p>
+                </div>
+                <div>
+                  <p className='text-4xl font-medium text-gray-800'>20+</p>
+                  <p> Mn. Sq. Ft. Delivered</p>
+                </div>
+                <div>
+                  <p className='text-4xl font-medium text-gray-800'>25+</p>
+                  <p> Ongoing Projects</p>
+                </div>
+              </div>
+              <p className='my-10 max-w-lg'>Real estate refers to land, buildings, and any natural or man-made structures
+                 attached to it. It includes residential, commercial, industrial, and 
+                 agricultural properties. The real estate industry involves buying, selling, renting,
+                  and developing properties for investment or personal use.</p>
+                  <button className='bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
             </div>
-            <div>
-                <p className='text 4xl font-medium text-gray-800'>12+</p>
-                <p>Projects Completed</p>
-            </div>
-            <div>
-                <p className='text 4xl font-medium text-gray-800'>20+</p>
-                <p>Mn. Sq. Ft. Delivered</p>
-            </div>
-            <div>
-                <p className='text 4xl font-medium text-gray-800'>25+</p>
-                <p>Ongoing Projects</p>
-            </div>
-        </div>
-      </div>
+         </div>
     </div>
   )
 }
