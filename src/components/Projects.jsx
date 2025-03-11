27cfc2dfc6f0ef371a5c463966423691bf6bf0c9
sueKK <125456919+sueKK@users.noreply.github.com>
2025-03-12 00:22:30 +0300

    Add project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { assets, projectsData } from '../assets/assets'
+import { reactHooks } from 'eslint-plugin-react-hooks';
+
+const Projects = () => {
+  return (
+    <div className='container mx-auto py-4 pt-20 px-6 md:px-20
+    lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+        <h1 className='text-2xl sm:text4xl font-bold mb-2 text-center'>Projects <span
+        className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting spaces, Building Legacies-Explore our Portfolio</p>
+
+        {/*slider buttons*/}
+
+        <div className='flex justify-end items-center mb-8'>
+            <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
+                <img src={assets.left_arrow} alt="Prvious" />
+            </button>
+            <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
+                <img src={assets.right_arrow} alt="Next" />
+            </button>
+        </div>
+
+        {/* Project slider container */}
+        <div className='overflow-hidden'>
+            <div className='flex gap-8 transition-transform duration-500
+            ease-in-out'>
+                {projectsData.map((project, index)=>(
+                    <div key={index} className='relative flex-shrink-0 
+                    w-full sm:w-1/4'>
+                        <img src={project.image} alt={project.title} />
+                        <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
+                            <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'> 
+                                <h2 className='text-xl font-semibold text-gray-800'>
+                                    {project.title}
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Projects
