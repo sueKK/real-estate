5fa8dfb035742243547f2a5a6586f8fafd52c465
sueKK <125456919+sueKK@users.noreply.github.com>
2025-04-14 16:00:39 +0300

    add Contact form

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { testimonialsData } from '../assets/assets'
+import { assets, testimonialsData } from '../assets/assets'
 
 const Testimonials = () => {
   return (
@@ -13,9 +13,17 @@ const Testimonials = () => {
 
 <div className='flex flex-wrap justify-center gap-8'>
     {testimonialsData.map((testimonial, index)=>(
-        <div key={index} className=' max-w-[340] border shadow-lg rounded px-8 py-12
+        <div key={index} className=' max-w-[340px] border shadow-lg rounded px-8 py-12
         text-center'>
             <img className='w-20 h-20 rounded-full mx-auto mb-4' src={testimonial.image} alt={testimonial.alt} />
+            <h2 className='text-xl text-gray-700 font-medium'>{testimonial.name}</h2>
+            <p className='text-gray-500 mb-4 text-sm'>{testimonial.title}</p>
+            <div className='flex justify-center gap-1 text-red-500 mb-4'>
+              {Array.from({length: testimonial.rating}, (item, index)=>(
+                <img key={index} src={assets.star_icon} alt="" />
+              ))}
+            </div>
+            <p className='text-gray-600'>{testimonial.text}</p>
         </div>
     ))}
 </div>
