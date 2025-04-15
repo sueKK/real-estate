4b055fd20468d16a8a3217a26f2f9fe396e93925
sueKK <125456919+sueKK@users.noreply.github.com>
2025-04-15 19:59:11 +0300

    Banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center
     flex items-center w-full overflow-hidden' style=
-    {{backgroundImage:"url('/header_img.jpg')"}} id='Header'>
+    {{backgroundImage:"url('/header_img.png')"}} id='Header'>
         <Navbar/>
         <motion.div 
         initial={{opacity:0, y:100}}
